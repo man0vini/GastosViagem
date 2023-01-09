3e26b36398f5d8c668b05125993f8c895e24b548
man0vini <mv.viniciusrocha3@gmail.com>
2023-01-09 19:39:50 -0300

    Calculation Logic

--- app/src/main/java/com/example/gastosviagem/MainActivity.kt
+++ app/src/main/java/com/example/gastosviagem/MainActivity.kt
@@ -2,9 +2,11 @@ package com.example.gastosviagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.example.gastosviagem.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -12,10 +14,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.buttonCalculate.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.button_calculate) {
+            calculate()
+        }
     }
     private fun calculate (){
+        val distance = binding.editDistance.text.toString().toFloat()
+        val price = binding.editPrice.text.toString().toFloat()
+        val autonomy = binding.editAutonomy.text.toString().toFloat()
+
+        val totalValue = (distance * price) / autonomy
+        binding.textTotalValue.text = "R$ ${"%.2f".format(totalValue)}"
+
+        //Toast.makeText( this,totalValue.toString(), Toast.LENGTH_SHORT).show()
+
 
     }
+
+
 }
 
 
